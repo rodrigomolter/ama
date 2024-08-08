9f180581a524e147064d1a710486bcc3c1f3b1ef
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-08-07 23:35:59 -0300

    :recycle: refactor: abstracting messages from room page

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom"
-import { ArrowRight, ArrowUp, Share2 } from "lucide-react"
+import { ArrowRight, Share2 } from "lucide-react"
 import { toast } from "sonner"
 
 import amaLogo from '../assets/ama-logo.svg'
+import { Message } from "../components/message"
 
 export function Room() {
   const { roomId } = useParams()
@@ -61,20 +62,9 @@ export function Room() {
       </form>
 
       <ol className="list-decimal list-outside px-3 space-y-8">
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          O que é GoLang e quais são suas principais vantagens em comparação com outras linguagens de programação como Python, Java ou C++?
-          <button type="button" className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500">
-            <ArrowUp className="size-4" />
-            Curtir pergunta (13)
-          </button>
-        </li>
-        <li className="ml-4 leading-relaxed text-zinc-100">
-          O que é GoLang e quais são suas principais vantagens em comparação com outras linguagens de programação como Python, Java ou C++?
-          <button type="button" className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300">
-            <ArrowUp className="size-4" />
-            Curtir pergunta (2)
-          </button>
-      </li>
+        <Message text="Como funcionam as goroutines em GoLang e por que elas são importantes para a concorrência e paralelismo?" amountOfReactions={100} answered />
+        <Message text="Quais são as melhores práticas para organizar o código em um projeto GoLang, incluindo pacotes, módulos e a estrutura de diretórios?" amountOfReactions={50} />
+        <Message text="Como fazer a depuração de programas GoLang e quais ferramentas são recomendadas para isso?" amountOfReactions={10} />
       </ol>
     </div>
   )
